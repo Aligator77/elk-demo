d06915f9e75672dd50766aa618a6f56821a5f689
Dan Grabowski <dan.grabowski@gmail.com>
2014-11-15 12:22:16 -0500

    improved distribution of ip addresses in generated data

--- generate-logs.sh
+++ generate-logs.sh
@@ -3,7 +3,7 @@
 # Generate log data to index
 event_count=100000
 echo "Generating access log file with ${event_count} entries..."
-java -jar lib/log-gen-0.1.0-standalone.jar ${event_count} var/logs/input/access-server1.log.tmp
+java -jar lib/log-gen-0.2.0-standalone.jar ${event_count} var/logs/input/access-server1.log.tmp
 echo "Sorting access log into chronological order..."
 time sort var/logs/input/access-server1.log.tmp > var/logs/input/access-server1.log
 rm var/logs/input/access-server1.log.tmp
