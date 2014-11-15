bc0fc1f2fc0e28847e25e3b7a88ff419313d430f
Dan Grabowski <dan.grabowski@gmail.com>
2014-11-14 23:26:29 -0500

    added script to reset data to initial state

---
+++ reset-data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pid=$(ps -o pid,command -e | grep elasticsearch | grep "bin/java" | awk '{printf "%d\n", $1}')
+if [ -n "${pid}" ]; then
+  echo "Elasticsearch is running as process ${pid}. Please stop it before resetting data."
+  exit 1
+fi
+
+pid=$(ps -o pid,command -e | grep logstash | grep "bin/java" | awk '{printf "%d\n", $1}')
+if [ -n "${pid}" ]; then
+  echo "Logstash is running as process ${pid}. Please stop it before resetting data."
+  exit 1
+fi
+
+pid=$(ps -o pid,command -e | grep node | grep kibana | awk '{printf "%d\n", $1}')
+if [ -n "${pid}" ]; then
+  echo "Kibana web server is running as process ${pid}. Please stop it before resetting data."
+  exit 1
+fi
+
+for dir in var/data/elasticsearch var/data/logstash var/logs/elasticsearch var/logs/logstash var/logs/kibana; do
+  rm -rf ${dir}
+  mkdir -p ${dir}
+done
